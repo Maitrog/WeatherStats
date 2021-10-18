c5fc3d579330132b83741e9f34f2fa2030446195
Maitrog <35650757+Maitrog@users.noreply.github.com>
2021-10-18 12:45:41 +0300

    Create update database scene

---
+++ src/main/java/com/maitrog/controllers/UpdateDatabaseController.java
@@ -0,0 +1,69 @@
+package com.maitrog.controllers;
+
+import com.jfoenix.controls.JFXButton;
+import com.maitrog.models.City;
+import com.maitrog.models.DbWeather;
+import com.maitrog.models.Parser;
+import com.maitrog.models.Weather;
+import com.maitrog.weatherstats.Main;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ProgressBar;
+import javafx.scene.text.Text;
+
+import java.io.IOException;
+import java.net.URL;
+import java.sql.SQLException;
+import java.util.List;
+import java.util.ResourceBundle;
+import java.util.logging.Level;
+
+public class UpdateDatabaseController implements Initializable {
+    @FXML
+    private ProgressBar progressBar;
+
+    @FXML
+    private Text lastCheckDate;
+
+    private static int lastCheckedCityId = 0;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+
+    public void updateDatabase(ActionEvent event) {
+        Main.logger.log(Level.INFO, "Start update database");
+        Thread update = new Thread(() -> {
+            try {
+                progressBar.setProgress(0);
+                List<City> cities = DbWeather.getInstance().getAllCities();
+                double step = 1.0 / cities.size();
+                for (City city :
+                        cities) {
+                    if (city.getId() > lastCheckedCityId) {
+                        List<Weather> weathers = Parser.parseYandexWeather(city.getUrlYandex());
+                        weathers.addAll(Parser.parseRambler(city.getUrlRambler()));
+                        weathers.addAll(Parser.parseWorldWeather(city.getUrlWorldWeather()));
+
+                        for (Weather weather : weathers) {
+                            weather.setCityId(city.getId());
+                            DbWeather.getInstance().addWeather(weather);
+                        }
+                        lastCheckedCityId = city.getId();
+                    }
+                    progressBar.setProgress(progressBar.getProgress() + step);
+                }
+                lastCheckedCityId = 0;
+                Main.logger.log(Level.INFO, "Database was updated");
+            } catch (SQLException | ClassNotFoundException | IOException e) {
+//                Main.logger.log(Level.SEVERE, String.format("Update database was crashed. Exception: %s", e.getMessage()));
+                Main.logger.log(Level.SEVERE, "Update database was crashed. Exception: ", e.getMessage());
+                e.printStackTrace();
+            }
+        });
+        update.setDaemon(true);
+        update.start();
+    }
+}
