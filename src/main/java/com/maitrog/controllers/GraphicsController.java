a53b3a139deaa0a7b24eaca521c78422be7bc84a
Tepminamop <fanbeyblade2001@gmail.com>
2021-10-19 17:08:56 +0300

    added combobox and textfield

--- src/main/java/com/maitrog/controllers/GraphicsController.java
+++ src/main/java/com/maitrog/controllers/GraphicsController.java
@@ -2,9 +2,10 @@ package com.maitrog.controllers;
 
 import com.jfoenix.controls.JFXComboBox;
 import com.jfoenix.controls.JFXHamburger;
-import javafx.fxml.FXML;
-import javafx.fxml.Initializable;
+import javafx.event.EventHandler;
+import javafx.fxml.*;
 
+import java.io.Console;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -18,11 +19,18 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.chart.LineChart;
+import javafx.scene.chart.NumberAxis;
+import javafx.scene.chart.XYChart;
 import javafx.scene.control.ProgressBar;
+import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.GridPane;
+import javafx.scene.shape.Line;
 import javafx.scene.text.TextAlignment;
 
 import java.io.IOException;
@@ -30,15 +38,39 @@ import java.net.URL;
 import java.sql.SQLException;
 import java.util.*;
 
+import javafx.stage.Stage;
 import org.jfree.chart.JFreeChart;
 
 public class GraphicsController implements Initializable {
 
     @FXML
-    private JFXComboBox comboBox;
+    private LineChart<String, Number> lineChart;
+
+    @FXML
+    private JFXButton loadButton;
+
+    @FXML
+    private JFXComboBox<String> comboBox;
+
+    @FXML
+    private TextField textField;
+
+    @FXML
+    private void handleButtonAction(ActionEvent e) {
+        lineChart.getData().clear();
+        XYChart.Series<String, Number> series = new XYChart.Series();
+        series.getData().add(new XYChart.Data<String, Number>("1", 2));
+        series.getData().add(new XYChart.Data<String, Number>("2", 1));
+        series.getData().add(new XYChart.Data<String, Number>("3", 4));
+
+        series.setName(comboBox.getValue() + ": " + textField.getText());
+        lineChart.getData().add(series);
+    }
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-
+        comboBox.getItems().add("Yandex");
+        comboBox.getItems().add("Rambler");
+        comboBox.getItems().add("WorldWeather");
     }
 }
