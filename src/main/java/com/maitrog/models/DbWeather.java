e20b14e89b23b7e86fcde67934742ca569c9fea1
Maitrog <35650757+Maitrog@users.noreply.github.com>
2021-11-30 14:30:28 +0300

    Add executing om background, refactor

--- src/main/java/com/maitrog/models/DbWeather.java
+++ src/main/java/com/maitrog/models/DbWeather.java
@@ -8,7 +8,6 @@ import java.io.IOException;
 import java.sql.*;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.Calendar;
 import java.util.Collections;
 import java.util.List;
 import java.util.logging.Level;
@@ -56,55 +55,6 @@ public class DbWeather {
         }
     }
 
-    public List<City> getAllCitiesWithWeather() {
-        try (Statement statement = connection.createStatement()) {
-            List<City> cities = getAllCities();
-            for (City city :
-                    cities) {
-                List<Weather> weathers = getWeathers(city.getId());
-                city.addWeather(weathers);
-            }
-            return cities;
-        } catch (SQLException throwables) {
-            Main.logger.log(Level.SEVERE, throwables.getMessage());
-            throwables.printStackTrace();
-            return Collections.emptyList();
-        }
-    }
-
-    public List<Weather> getAllWeathers() {
-        try (Statement statement = connection.createStatement()) {
-            ResultSet weatherResult = statement.executeQuery("SELECT * FROM Weather");
-            return parseWeatherResponse(weatherResult);
-        } catch (SQLException throwables) {
-            Main.logger.log(Level.SEVERE, throwables.getMessage());
-            throwables.printStackTrace();
-            return Collections.emptyList();
-        }
-    }
-
-    public List<Weather> getWeathers(int cityId) {
-        try (Statement statement = connection.createStatement()) {
-            ResultSet weatherResult = statement.executeQuery(String.format("SELECT * FROM Weather WHERE CityId = %d ORDER BY(CheckedDate)", cityId));
-            return parseWeatherResponse(weatherResult);
-        } catch (SQLException throwables) {
-            Main.logger.log(Level.SEVERE, throwables.getMessage());
-            throwables.printStackTrace();
-            return Collections.emptyList();
-        }
-    }
-
-    public List<Weather> getWeathers(int cityId, SiteType siteType) {
-        try (Statement statement = connection.createStatement()) {
-            ResultSet weatherResult = statement.executeQuery(String.format("SELECT * FROM Weather WHERE CityId = %d AND SiteType = %d ORDER BY(CheckedDate)", cityId, SiteType.getValue(siteType)));
-            return parseWeatherResponse(weatherResult);
-        } catch (SQLException throwables) {
-            Main.logger.log(Level.SEVERE, throwables.getMessage());
-            throwables.printStackTrace();
-            return Collections.emptyList();
-        }
-    }
-
     public List<Weather> getWeathers(String name, SiteType siteType, Date targetDate) {
         SimpleDateFormat sdf = new SimpleDateFormat("dd.MM.yyyy");
         try (Statement statement = connection.createStatement()) {
@@ -121,7 +71,6 @@ public class DbWeather {
             return Collections.emptyList();
         }
     }public List<List<Weather>> getSortedWeathers(int cityId, SiteType siteType) {
-        SimpleDateFormat sdf = new SimpleDateFormat("dd.MM.yyyy");
         try (Statement statement = connection.createStatement()) {
             ResultSet weatherResult = statement.executeQuery(String.format("""
                             SELECT *
@@ -152,39 +101,20 @@ public class DbWeather {
         }
     }
 
-    public List<Date> getTargetDate(int cityId) {
-        SimpleDateFormat sdf = new SimpleDateFormat("dd.MM.yyyy");
-        try (Statement statement = connection.createStatement()) {
-            ResultSet dateResult = statement.executeQuery(String.format("SELECT DISTINCT TargetDate\n" +
-                            "FROM Weather\n" +
-                            "WHERE CityId = %d AND TargetDate = CheckedDate\n" +
-                            "ORDER BY(TargetDate)",
-                    cityId));
-            List<Date> dates = new ArrayList<>();
-            while (dateResult.next()) {
-                dates.add(dateResult.getDate("TargetDate"));
-            }
-            return dates;
-        } catch (SQLException throwables) {
-            Main.logger.log(Level.SEVERE, throwables.getMessage());
-            throwables.printStackTrace();
-            return Collections.emptyList();
-        }
-    }
-
     public List<Double> getAllAvgTemperature(int cityId){
         try (Statement statement = connection.createStatement()) {
-            ResultSet avgTempResult = statement.executeQuery(String.format("WITH allTemp as ((SELECT MinTemperature as Temperature, TargetDate\n" +
-                            "FROM Weather\n" +
-                            "WHERE CityId = %d AND TargetDate = CheckedDate)\n" +
-                            "UNION ALL\n" +
-                            "(SELECT MaxTemperature as Temperature, TargetDate\n" +
-                            "FROM Weather\n" +
-                            "Where CityId = %d AND TargetDate = CheckedDate))\n" +
-                            "SELECT AVG(Cast(Temperature as Float)) as avgTemp\n" +
-                            "FROM allTemp\n" +
-                            "GROUP BY(TargetDate)\n" +
-                            "ORDER BY(TargetDate)",
+            ResultSet avgTempResult = statement.executeQuery(String.format("""
+                            WITH allTemp as ((SELECT MinTemperature as Temperature, TargetDate
+                            FROM Weather
+                            WHERE CityId = %d AND TargetDate = CheckedDate)
+                            UNION ALL
+                            (SELECT MaxTemperature as Temperature, TargetDate
+                            FROM Weather
+                            Where CityId = %d AND TargetDate = CheckedDate))
+                            SELECT AVG(Cast(Temperature as Float)) as avgTemp
+                            FROM allTemp
+                            GROUP BY(TargetDate)
+                            ORDER BY(TargetDate)""",
                     cityId, cityId));
 
             List<Double> avgTemp = new ArrayList<>();
@@ -196,50 +126,6 @@ public class DbWeather {
             Main.logger.log(Level.SEVERE, throwables.getMessage());
             throwables.printStackTrace();
             return Collections.emptyList();
-        }
-    }
-
-    public double getAvgTemperature(int cityId, Date date) {
-        SimpleDateFormat sdf = new SimpleDateFormat("dd.MM.yyyy");
-        try (Statement statement = connection.createStatement()) {
-            ResultSet avgTempResult = statement.executeQuery(String.format("WITH allTemp as ((SELECT MinTemperature as Temperature\n" +
-                            "FROM Weather\n" +
-                            "WHERE CityId = %d AND TargetDate = '%s' AND CheckedDate = '%s')\n" +
-                            "UNION ALL\n" +
-                            "(SELECT MaxTemperature as Temperature\n" +
-                            "FROM Weather\n" +
-                            "Where CityId = %d AND TargetDate = '%s' AND CheckedDate = '%s'))\n" +
-                            "\n" +
-                            "SELECT AVG(Cast(Temperature as Float)) as avgTemp\n" +
-                            "FROM allTemp",
-                    cityId, sdf.format(date), sdf.format(date), cityId, sdf.format(date), sdf.format(date)));
-
-            double avgTemp = -9999;
-            while (avgTempResult.next()) {
-                avgTemp = avgTempResult.getDouble("avgTemp");
-            }
-            return avgTemp;
-        } catch (SQLException throwables) {
-            Main.logger.log(Level.SEVERE, throwables.getMessage());
-            throwables.printStackTrace();
-            return -9999;
-        }
-    }
-
-    public void addCity(City city) {
-        try (PreparedStatement statement = connection.prepareStatement(
-                "INSERT INTO Cities(NameRu, NameEn, UrlYandex, UrlRambler, UrlWorldWeather, Country) " +
-                        "VALUES (?, ?, ?, ?, ?, ?)")) {
-            statement.setObject(1, city.getNameRu());
-            statement.setObject(2, city.getNameEn());
-            statement.setObject(3, city.getUrlYandex());
-            statement.setObject(4, city.getUrlRambler());
-            statement.setObject(5, city.getUrlWorldWeather());
-            statement.setObject(6, city.getCountry());
-            statement.execute();
-        } catch (SQLException throwables) {
-            Main.logger.log(Level.SEVERE, throwables.getMessage());
-            throwables.printStackTrace();
         }
     }
 
